71583fac790b68eae822822f7501ab39e4fcf652
Reyca Dealba <reycadealba07192303-ai@gmail.com>
2026-02-28 09:47:29 +0800

    Allow Vercel frontend CORS in backend

--- backend/main.py
+++ backend/main.py
@@ -9,19 +9,9 @@ load_dotenv()
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    "http://localhost:8000",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5174",
-    "http://127.0.0.1:8000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
